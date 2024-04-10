7dca1d16bda5e919d56fb04967009513d1448e8a
Kevin Xiong <kxiong3@oakland.edu>
2024-04-10 02:03:02 -0400

    Revert "Merge branch 'main' of https://github.com/Thorfinn980/ProjectW"
    This reverts commit 930aabf508e10d66194092187e030f4bca926b5b, reversing
    changes made to db2d2d3fcc69c5739d0b03cafc05a80759ec511d.

--- scripts/player scripts ver2/Player.gd
+++ scripts/player scripts ver2/Player.gd
@@ -40,13 +40,9 @@ func _physics_process(delta):
 func update_animation():
 	anim_tree.set("parameters/AnimationStateMachine/Move/blend_position", direction.x)
 
-signal change_sword_direction(direction : bool)
-
 func update_direction():
 	if direction.x > 0:
 		sprite.flip_h = false
-		emit_signal("change_sword_direction", false)
 	elif direction.x < 0:
 		sprite.flip_h = true
-		emit_signal("change_sword_direction", true)
 		
